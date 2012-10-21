684d0ed0de511f30301526149c493fc47d89a40c
Veer Jain <jain.veerbahadur@gmail.com>
2012-10-21 14:15:26 +0530

    Write Message screen UI added

--- Resources/app/ui/CreateCard/writeMsg.js
+++ Resources/app/ui/CreateCard/writeMsg.js
@@ -27,7 +27,52 @@ function WriteMsg() {"use strict";
 		text : 'Write Message'
 	});
 	
+	this.postCardView = Ti.UI.createView({
+		backgroundImage:'app/assets/postcard.png',
+		width:290,
+		height:180,
+		top:10
+	});
+	
+	this.messageView = Ti.UI.createView({
+		height:120,
+		width:280
+	});
+	
+	this.addMsgView = Ti.UI.createView({
+		left:45,
+		height:60,
+		width:55,
+		backgroundImage : 'app/assets/addMessage.png'
+	});
+	
+	this.addMsgLabel = Ti.UI.createLabel({
+		top:90,
+		left:20,
+		text:'Add Message'
+	});
+	
+	this.addAddressView = Ti.UI.createView({
+		left:175,
+		height:56,
+		width:51,
+		backgroundImage : 'app/assets/addAddress.png'
+	});
+	
+	this.addAddressLabel = Ti.UI.createLabel({
+		top:90,
+		left:155,
+		text:'Add Address'
+	});
+	
+	this.messageView.add(this.addMsgView);
+	this.messageView.add(this.addAddressView);
+	this.messageView.add(this.addMsgLabel);
+	this.messageView.add(this.addAddressLabel);
+	
 	this.wrapper.add(this.writeMsgLabel);
+	this.wrapper.add(this.postCardView);
+	this.wrapper.add(this.messageView);
 }
 
 module.exports = WriteMsg;
