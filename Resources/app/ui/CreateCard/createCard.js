646047ab0939eb895237306fab881ecea2117498
Veer Jain <jain.veerbahadur@gmail.com>
2012-10-21 14:41:38 +0530

    Create card ui added

--- Resources/app/ui/CreateCard/createCard.js
+++ Resources/app/ui/CreateCard/createCard.js
@@ -27,7 +27,53 @@ function CreateCard() {"use strict";
 		text : 'Create Card'
 	});
 	
+	this.cardImageView = Ti.UI.createView({
+		backgroundColor:'#666666',
+		width:290,
+		height:180,
+		top:10
+	});
+	
+	this.imageOptionView = Ti.UI.createView({
+		height:120,
+		width:280
+	});
+	
+	this.editImageView = Ti.UI.createView({
+		left:30,
+		height:60,
+		width:58,
+		backgroundImage : 'app/assets/editImage.png'
+	});
+	
+	this.editImageLabel = Ti.UI.createLabel({
+		top:90,
+		left:25,
+		text:'Edit Image'
+	});
+	
+	this.changeImageView = Ti.UI.createView({
+		left:125,
+		height:60,
+		width:151,
+		backgroundImage : 'app/assets/changeImage.png'
+	});
+	
+	this.changeImageLabel = Ti.UI.createLabel({
+		top:90,
+		left:145,
+		text:'Change Image'
+	});
+	
+	this.imageOptionView.add(this.editImageView);
+	this.imageOptionView.add(this.editImageLabel);
+	this.imageOptionView.add(this.changeImageView);
+	this.imageOptionView.add(this.changeImageLabel);
+	
 	this.wrapper.add(this.createCardLabel);
+	this.wrapper.add(this.cardImageView);
+	this.wrapper.add(this.imageOptionView);
+	
 }
 
 module.exports = CreateCard;
